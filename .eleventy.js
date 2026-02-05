a6cd4782b6a80e73a84863087ef938131c9e42f7
logos777 <logos777@walla.com>
2026-02-05 03:25:59 +0000

    Updates

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+module.exports = function(eleventyConfig) {
+  // Custom filter to find an index by URL
+  eleventyConfig.addFilter("findIndexByUrl", (items, url) => {
+    if (!items || !url) return -1;
+    return items.findIndex(item => item.url === url);
+  });
+
+  // Passthrough copy for assets
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
+  };
+};
